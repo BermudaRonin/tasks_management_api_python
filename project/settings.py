aeaa8f12ae4107025aa8a9ff561b8883fd073fb1
Bermuda Ronin <bermudaronin@gmail.com>
2025-01-03 19:07:01 +0100

    :sparkles: Switch to PostgreSQL

--- project/settings.py
+++ project/settings.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 import os
 
+ENVIRONMENT = os.getenv("ENVIRONMENT", "DEV")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("DJ_SECRET_KEY")
+SECRET_KEY = os.getenv("DJ_SECRET_KEY") 
 
-if os.environ.get("ENVIRONMENT") == "PROD":
+if ENVIRONMENT == "PROD":
     DEBUG = False
 else:
     DEBUG = True
@@ -62,9 +63,13 @@ WSGI_APPLICATION = "project.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
